c43df62947bd10c2f573170aea9130dd65b1f283
Biniyam Abebe <Biniyamabe16@gmail.com>
2023-11-22 11:19:55 +0300

    check

---
+++ index.js
@@ -0,0 +1,17 @@
+var arr = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var arr2 = [];
+var n = 0;
+
+const func = () => {
+  for (var i = arr.length-1; i >= 0; i--) {
+    arr2[n] = arr[i];
+    n += 1;
+  }
+}
+func();
+
+for (var i = 0; i <= arr2.length; i++) {
+    var a = arr2[i];
+    var b = arr[i];
+    console.log("[ "+a+" - "+b+" ]")
+}
